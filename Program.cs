b8e7bdd46a342e180a1006e94dc93b8e64f5a6d9
domeata <03maydom@rgtfo-me.it>
2020-04-16 12:51:37 +0200

    Basic calculation added

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace FirstGitTry
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("Second Change");
+
+            int lenghta = 25;
+            int lenghtb = 25;
+            int solution = lenghta + lenghtb;
+
+            Console.WriteLine(solution);
         }
     }
 }
